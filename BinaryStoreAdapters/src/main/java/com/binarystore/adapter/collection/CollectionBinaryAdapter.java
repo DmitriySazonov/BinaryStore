11b55945f875092fd2de968b62f0a8cf3c6b2449
Nikita Vanyushin <garborezki@yandex-team.ru>
2021-05-09 13:10:51 +0300

    fix collection adapter

--- BinaryStoreAdapters/src/main/java/com/binarystore/adapter/collection/CollectionBinaryAdapter.java
+++ BinaryStoreAdapters/src/main/java/com/binarystore/adapter/collection/CollectionBinaryAdapter.java
@@ -1,14 +1,17 @@
 package com.binarystore.adapter.collection;
 
+import com.binarystore.VersionException;
 import com.binarystore.adapter.AbstractBinaryAdapter;
 import com.binarystore.adapter.BinaryAdapter;
 import com.binarystore.adapter.BinaryAdapterProvider;
 import com.binarystore.adapter.Key;
 import com.binarystore.adapter.NullBinaryAdapter;
 import com.binarystore.adapter.UnknownItemStrategy;
+import com.binarystore.adapter.map.AbstractMapBinaryAdapter;
 import com.binarystore.buffer.ByteBuffer;
 
 import java.util.Collection;
+import java.util.Map;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -19,16 +22,25 @@ public abstract class CollectionBinaryAdapter<T extends Collection> extends Abst
     @Nonnull
     private final CollectionSettings settings;
     private final BinaryAdapterProvider adapterProvider;
+    private final byte version = 1;
 
     protected class Adapters {
+        Key<?> lastValueKey = null;
         Class<?> lastValueClass = null;
         BinaryAdapter<Object> lastValueAdapter = null;
 
         void setValueClass(@CheckForNull Object value) throws Exception {
-            final Class<?> valueClass = value != null ? value.getClass() : null;
+            final Class<?> valueClass = value != null ? value.getClass() : NullBinaryAdapter.NULL_CLASS;
             if (valueClass != lastValueClass) {
                 lastValueClass = valueClass;
                 lastValueAdapter = getAdapterForClass(valueClass);
+            }
+        }
+
+        void setValueKey(Key<?> valueClass) throws Exception {
+            if (!valueClass.equals(lastValueKey)) {
+                lastValueKey = valueClass;
+                lastValueAdapter = getAdapterForKey(valueClass);
             }
         }
     }
@@ -49,59 +61,59 @@ public abstract class CollectionBinaryAdapter<T extends Collection> extends Abst
         int accumulator = key().getSize();
         int elementCount = 0;
         for (Object element : value) {
+            adapters.setValueClass(element);
             if (checkForNull(adapters.lastValueAdapter, value)) {
                 continue;
             }
+            int itemSize = 0;
             try {
-                adapters.setValueClass(element.getClass());
+                itemSize += adapters.lastValueAdapter.key().getSize();
+                itemSize += adapters.lastValueAdapter.getSize(element);
             } catch  (Throwable throwable) {
                 if (settings.exceptionItemStrategy == UnknownItemStrategy.THROW_EXCEPTION) {
                     throw new IllegalStateException("Fail getSize for class" + element.getClass());
                 }
                 continue;
             }
-            accumulator += adapters.lastValueAdapter.key().getSize();
+            accumulator += itemSize;
             elementCount++;
         }
         return (ByteBuffer.BYTE_BYTES +  // version
                 ByteBuffer.INTEGER_BYTES + // collection size
-                getSizeAdditionalMeta(value) + // additional meta for heir
                 ByteBuffer.INTEGER_BYTES +  // offset to meta
                 accumulator + // data size
-                ByteBuffer.INTEGER_BYTES * elementCount // entries offsets
+                ByteBuffer.INTEGER_BYTES * elementCount // collection offsets
         );
     }
 
     @Override
     public void serialize(@Nonnull ByteBuffer byteBuffer, @Nonnull T value) throws Exception {
         int index = 0;
-        int collectionSize = value.size();
         final Adapters adapters = new Adapters();
-        final int[] offsets = new int[collectionSize];
-        byteBuffer.write(collectionSize);
+        final int[] offsets = new int[value.size()];
+        byteBuffer.write(version);
         final int startOffset = byteBuffer.getOffset();
         byteBuffer.moveOffset(ByteBuffer.INTEGER_BYTES); // space for size
         byteBuffer.moveOffset(ByteBuffer.INTEGER_BYTES); // space for offset to meta
-        serializeAdditionalMeta(byteBuffer, value); // see in heir
         for (Object element : value) {
-            offsets[index++] = byteBuffer.getOffset();
+            final int offset = byteBuffer.getOffset();
             if (checkForNull(adapters.lastValueAdapter, value)) {
                 continue;
             }
             try {
-                adapters.setValueClass(element.getClass());
+                adapters.lastValueAdapter.key().saveTo(byteBuffer);
+                adapters.lastValueAdapter.serialize(byteBuffer, element);
+                offsets[index++] = offset;
             } catch  (Throwable throwable) {
                 if (settings.exceptionItemStrategy == UnknownItemStrategy.THROW_EXCEPTION) {
                     throw new IllegalStateException("Fail getSize for class" + element.getClass());
                 }
-                continue;
+                byteBuffer.setOffset(offset);
             }
-            adapters.lastValueAdapter.key().saveTo(byteBuffer);
-            adapters.lastValueAdapter.serialize(byteBuffer, element);
         }
         final int endDataOffset = byteBuffer.getOffset();
         byteBuffer.setOffset(startOffset);
-        byteBuffer.write(index); // write actual size of map
+        byteBuffer.write(index); // write actual size of collection
         byteBuffer.write(endDataOffset); // write offset to start of meta
         byteBuffer.setOffset(endDataOffset); // move to the end to write meta
         for (int i = 0; i < index; i++) {
@@ -114,14 +126,17 @@ public abstract class CollectionBinaryAdapter<T extends Collection> extends Abst
     @Override
     public T deserialize(@Nonnull ByteBuffer byteBuffer) throws Exception {
         final Adapters adapters = new Adapters();
+        final byte version = byteBuffer.readByte();
+        if (this.version != version) {
+            throw new VersionException(this.version, version);
+        }
         final int size = byteBuffer.readInt();
-        final int startDataOffset = byteBuffer.readInt();
+        byteBuffer.moveOffset(ByteBuffer.INTEGER_BYTES); // skip offest to meta
         final T collection = createCollection(size);
         final Collection<Object> mutableCollection = (Collection<Object>) collection;
-        byteBuffer.setOffset(startDataOffset);
         for (int i = 0; i < size; i++) {
             final Key valueKey = Key.read(byteBuffer);
-            adapters.setValueClass(valueKey);
+            adapters.setValueKey(valueKey);
             if (checkForNull(adapters.lastValueAdapter, valueKey)) {
                 continue;
             }
@@ -129,14 +144,6 @@ public abstract class CollectionBinaryAdapter<T extends Collection> extends Abst
         }
         byteBuffer.moveOffset(ByteBuffer.INTEGER_BYTES * size);
         return collection;
-    }
-
-    protected int getSizeAdditionalMeta(@Nonnull T value) throws Exception {
-        return 0;
-    }
-
-    protected void serializeAdditionalMeta(@Nonnull ByteBuffer buffer, @Nonnull T value) throws Exception {
-
     }
 
     private boolean checkForNull(@CheckForNull BinaryAdapter<?> adapter, @Nonnull Object key) {
@@ -153,17 +160,24 @@ public abstract class CollectionBinaryAdapter<T extends Collection> extends Abst
         return adapter == null;
     }
 
-    @Nonnull
     @SuppressWarnings("unchecked")
-    protected BinaryAdapter<Object> getAdapterForClass(Class<?> clazz) throws Exception {
+    private BinaryAdapter<Object> getAdapterForKey(@Nonnull Key<?> key) throws Exception {
         final BinaryAdapter<?> adapter;
-        if (clazz == null) {
+        if (key.equals(NullBinaryAdapter.instance.key())) {
+            adapter = NullBinaryAdapter.instance;
+        } else {
+            adapter = adapterProvider.getAdapterByKey(key, null);
+        }
+        return (BinaryAdapter<Object>) adapter;
+    }
+
+    @SuppressWarnings("unchecked")
+    private BinaryAdapter<Object> getAdapterForClass(@Nonnull Class<?> clazz) throws Exception {
+        final BinaryAdapter<?> adapter;
+        if (clazz == NullBinaryAdapter.NULL_CLASS) {
             adapter = NullBinaryAdapter.instance;
         } else {
             adapter = adapterProvider.getAdapterForClass(clazz, null);
-        }
-        if (adapter == null) {
-            throw new IllegalArgumentException("Couldn't find adapter for class " + clazz);
         }
         return (BinaryAdapter<Object>) adapter;
     }
